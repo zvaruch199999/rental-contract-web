15b7a3d29ef8c8a63b45fe4428190292cdfef784
zvaruch199999 <zvaruch199999@gmail.com>
2025-08-28 01:34:31 +0200

    Add files via upload
    /** @type {import('next').NextConfig} */
    const nextConfig = { reactStrictMode: true };
    module.exports = nextConfig;

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: true }
-};
-module.exports = nextConfig;
+const nextConfig={reactStrictMode:true};
+module.exports=nextConfig;
