2f65f7e5e956eaba46a38a264d2049cfb2f555ad
zvaruch199999 <zvaruch199999@gmail.com>
2025-08-28 02:09:09 +0200

    Update AuthGate.tsx

--- AuthGate.tsx
+++ AuthGate.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { useEffect, useState } from 'react'
 import { onAuthStateChanged, User } from 'firebase/auth'
-- import { auth, signInWithGoogle, signOutUser } from '@/lib/firebase'
-+ import { auth, signInWithGoogle, signOutUser } from './firebase'
+import { auth, signInWithGoogle, signOutUser } from '@/lib/firebase'
+import { auth, signInWithGoogle, signOutUser } from './firebase'
 
 
 export default function AuthGate({ children }: { children: React.ReactNode }) {
